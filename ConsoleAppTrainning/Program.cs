50b77ab7f636f0eb032b2d1a7daa125c69cd2df3
jackson-ya <jackdimitrov@gmail.com>
2024-11-14 19:26:58 +0200

    master/slave

--- ConsoleAppTrainning/Program.cs
+++ ConsoleAppTrainning/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppTrainning.Types;
+
+
+var number = new Numbers();
+
+Console.WriteLine(number.firstNumbers);
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine(10 - 1);
---
+++ ConsoleAppTrainning/Types/Numbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTrainning.Types
+{
+    internal class Numbers
+    {
+        public int firstNumbers = 5;
+    }
+}
